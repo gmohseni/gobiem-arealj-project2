305041d7fd993c4b5dfe4cba02b247316cc1ffd0
gmohseni <47803833+gmohseni@users.noreply.github.com>
2021-03-13 10:28:40 -0800

    Added Shape Objects

--- src/components/Shapes.js
+++ src/components/Shapes.js
@@ -1,5 +1,173 @@
-import test from '../svgs/threesolidstars.png';
+import one from '../imgs/oneblueclearcircle.png';
+import two from '../imgs/oneblueclearrectangle.png';
+import three from '../imgs/oneblueclearsquare.png';
+import four from '../imgs/onebluepatterncircle.png';
+import five from '../imgs/onebluepatternrectangle.png';
+import six from '../imgs/onebluepatternsquare.png';
+import seven from '../imgs/onebluesolidcircle.png';
+import eight from '../imgs/onebluesolidrectangle.png';
+import nine from '../imgs/onebluesolidsquare.png';
+import ten from '../imgs/onegreenclearcircle.png';
+import eleven from '../imgs/onegreenclearrectangle.png';
+import twelve from '../imgs/onegreenclearsquare.png';
+import thirteen from '../imgs/onegreenpatterncircle.png';
+import fourteen from '../imgs/onegreenpatternrectangle.png';
+import fifteen from '../imgs/onegreenpatternsquare.png';
+import sixteen from '../imgs/onegreensolidcircle.png';
+import seventy from '../imgs/onegreensolidrectangle.png';
+import eighteen from '../imgs/onegreensolidsquare.png';
+import nineteen from '../imgs/oneredclearcircle.png';
+import twenty from '../imgs/oneredclearrectangle.png';
+import twentyOne from '../imgs/oneredclearsquare.png';
+import twentyTwo from '../imgs/oneredpatterncircle.png';
+import twentyThree from '../imgs/oneredpatternrectangle.png';
+import twentyFour from '../imgs/oneredpatternsquare.png';
+import twentyFive from '../imgs/oneredsolidcircle.png';
+import twentySix from '../imgs/oneredsolidrectangle.png';
+import twentySeven from '../imgs/oneredsolidsquare.png';
+
+
+import twentyEight from '../imgs/threeblueclearcircles.png';
+import twentyNine from '../imgs/threeblueclearrectangles.png';
+import thirty from '../imgs/threeblueclearsquares.png';
+import thirtyOne from '../imgs/threebluepatterncircles.png';
+import thirtyTwo from '../imgs/threebluepatternrectangles.png';
+import thirtyThree from '../imgs/threebluepatternsquares.png';
+import thirtyFour from '../imgs/threebluesolidcircles.png';
+import thirtyFive from '../imgs/threebluesolidrectangles.png';
+import thirtySix from '../imgs/threebluesolidsquares.png';
+import thirtySeven from '../imgs/threegreenclearcircles.png';
+import thirtyEight from '../imgs/threegreenclearrectangles.png';
+import thirtyNine from '../imgs/threegreenclearsquares.png';
+import forty from '../imgs/threegreenpatterncircles.png';
+import fortyOne from '../imgs/threegreenpatternrectangles.png';
+import fortyTwo from '../imgs/threegreenpatternsquares.png';
+import fortyThree from '../imgs/threegreensolidcircles.png';
+import fortyFour from '../imgs/threegreensolidrectangles.png';
+import fortyFive from '../imgs/threegreensolidsquares.png';
+import fortySix from '../imgs/threeredclearcircles.png';
+import fortySeven from '../imgs/threeredclearrectangles.png';
+import fortyEight from '../imgs/threeredclearsquares.png';
+import fortyNine from '../imgs/threeredpatterncircles.png';
+import fifty from '../imgs/threeredpatternrectangles.png';
+import fiftyOne from '../imgs/threeredpatternsquares.png';
+import fiftyTwo from '../imgs/threeredsolidcircles.png';
+import fiftyThree from '../imgs/threeredsolidrectangles.png';
+import fiftyFour from '../imgs/threeredsolidsquares.png';
+
+
+import fiftyFive from '../imgs/twoblueclearcircles.png';
+import fiftySix from '../imgs/twoblueclearrectangles.png';
+import fiftySeven from '../imgs/twoblueclearsquares.png';
+import fiftyEight from '../imgs/twobluepatterncircles.png';
+import fiftyNine from '../imgs/twobluepatternrectangles.png';
+import sixty from '../imgs/twobluepatternsquares.png';
+import sixtyOne from '../imgs/twobluesolidcircles.png';
+import sixtyTwo from '../imgs/twobluesolidrectangles.png';
+import sixtyThree from '../imgs/twobluesolidsquares.png';
+import sixtyFour from '../imgs/twogreenclearcircles.png';
+import sixtyFive from '../imgs/twogreenclearrectangles.png';
+import sixtySix from '../imgs/twogreenclearsquares.png';
+import sixtySeven from '../imgs/twogreenpatterncircles.png';
+import sixtyEight from '../imgs/twogreenpatternrectangles.png';
+import sixtyNine from '../imgs/twogreenpatternsquares.png';
+import seventy from '../imgs/twogreensolidcircles.png';
+import seventyOne from '../imgs/twogreensolidrectangles.png';
+import seventyTwo from '../imgs/twogreensolidsquares.png';
+import seventyThree from '../imgs/tworedclearcircles.png';
+import seventyFour from '../imgs/tworedclearrectangles.png';
+import seventyFive from '../imgs/tworedclearsquares.png';
+import seventySix from '../imgs/tworedpatterncircles.png';
+import seventySeven from '../imgs/tworedpatternrectangles.png';
+import seventyEight from '../imgs/tworedpatternsquares.png';
+import seventyNine from '../imgs/tworedsolidcircles.png';
+import eighty from '../imgs/tworedsolidrectangles.png';
+import eightyOne from '../imgs/tworedsolidsquares.png';
 
 export let Shapes = [
-    {url: test}
+    {url: one, id: 1, description: "oneBlueClearCircle"},
+    {url: two, id: 2, description: "oneBlueClearRectangle"},
+    {url: three, id: 3, description: "oneBlueClearSquare"},
+    {url: four, id: 4, description: "oneBluePatternCircle"},
+    {url: five, id: 5, description: "oneBluePatternRectangle"},
+    {url: six, id: 6, description: "oneBluePatternSquare"},
+    {url: seven, id: 7, description: "oneBlueSolidCircle"},
+    {url: eight, id: 8, description: "oneBlueSolidRectangle"},
+    {url: nine, id: 9, description: "oneBlueSolidSquare"},
+    {url: ten, id: 10, description: "oneGreenClearCircle"},
+    {url: eleven, id: 11, description: "oneGreenClearRectangle"},
+    {url: twelve, id: 12, description: "oneGreenClearSquare"},
+    {url: thirteen, id: 13, description: "oneGreenPatternCircle"},
+    {url: fourteen, id: 14, description: "oneGreenPatternRectangle"},
+    {url: fifteen, id: 15, description: "oneGreenPatternSquare"},
+    {url: sixteen, id: 16, description: "oneGreenSolidCircle"},
+    {url: seventeen, id: 17, description: "oneGreenSolidRectangle"},
+    {url: eighteen, id: 18, description: "oneGreenSolidSquare"},
+    {url: nineteen, id: 19, description: "oneRedClearCircle"},
+    {url: twenty, id: 20, description: "oneRedClearRectangle"},
+    {url: twentyOne, id: 21, description: "oneRedClearSquare"},
+    {url: twentyTwo, id: 22, description: "oneRedPatternCircle"},
+    {url: twentyThree, id: 23, description: "oneRedPatternRectangle"},
+    {url: twentyFour, id: 24, description: "oneRedPatternSquare"},
+    {url: twentyFive, id: 25, description: "oneRedSolidCircle"},
+    {url: twentySix, id: 26, description: "oneRedSolidRectangle"},
+    {url: twentySeven, id: 27, description: "oneRedSolidSquare"},
+
+    {url: twentyEight, id: 28, description: "threeBlueClearCircle"},
+    {url: twentyNine, id: 29, description: "threeBlueClearRectangle"},
+    {url: thirty, id: 30, description: "threeBlueClearSquare"},
+    {url: thirtyOne, id: 31, description: "threeBluePatternCircle"},
+    {url: thirtyTwo, id: 32, description: "threeBluePatternRectangle"},
+    {url: thirtyThree, id: 33, description: "threeBluePatternSquare"},
+    {url: thirtyFour, id: 34, description: "threeBlueSolidCircle"},
+    {url: thirtyFive, id: 35, description: "threeBlueSolidRectangle"},
+    {url: thirtySix, id: 36, description: "threeBlueSolidSquare"},
+    {url: thirtySeven, id: 37, description: "threeGreenClearCircle"},
+    {url: thirtyEight, id: 38, description: "threeGreenClearRectangle"},
+    {url: thirtyNine, id: 39, description: "threeGreenClearSquare"},
+    {url: forty, id: 40, description: "threeGreenPatternCircle"},
+    {url: fortyOne, id: 41, description: "threeGreenPatternRectangle"},
+    {url: fortyTwo, id: 42, description: "threeGreenPatternSquare"},
+    {url: fortyThree, id: 43, description: "threeGreenSolidCircle"},
+    {url: fortyFour, id: 44, description: "threeGreenSolidRectangle"},
+    {url: fortyFive, id: 45, description: "threeGreenSolidSquare"},
+    {url: fortySix, id: 46, description: "threeRedClearCircle"},
+    {url: fortySeven, id: 47, description: "threeRedClearRectangle"},
+    {url: fortyEight, id: 48, description: "threeRedClearSquare"},
+    {url: fortyNine, id: 49, description: "threeRedPatternCircle"},
+    {url: fifty, id: 50, description: "threeRedPatternRectangle"},
+    {url: fiftyOne, id: 51, description: "threeRedPatternSquare"},
+    {url: fiftyTwo, id: 52, description: "threeRedSolidCircle"},
+    {url: fiftyThree, id: 53, description: "threeRedSolidRectangle"},
+    {url: fiftyFour, id: 54, description: "threeRedSolidSquare"},
+    
+    {url: fiftyFive, id: 55, description: "twoBlueClearCircle"},
+    {url: fiftySix, id: 56, description: "twoBlueClearRectangle"},
+    {url: fiftySeven, id: 57, description: "twoBlueClearSquare"},
+    {url: fiftyEight, id: 58, description: "twoBluePatternCircle"},
+    {url: fiftyNine, id: 59, description: "twoBluePatternRectangle"},
+    {url: sixty, id: 60, description: "twoBluePatternSquare"},
+    {url: sixtyOne, id: 61, description: "twoBlueSolidCircle"},
+    {url: sixtyTwo, id: 62, description: "twoBlueSolidRectangle"},
+    {url: sixtyThree, id: 63, description: "twoBlueSolidSquare"},
+    {url: sixtyFour, id: 64, description: "twoGreenClearCircle"},
+    {url: sixtyFive, id: 65, description: "twoGreenClearRectangle"},
+    {url: sixtySix, id: 66, description: "twoGreenClearSquare"},
+    {url: sixtySeven, id: 67, description: "twoGreenPatternCircle"},
+    {url: sixtyEight, id: 68, description: "twoGreenPatternRectangle"},
+    {url: sixtyNine, id: 69, description: "twoGreenPatternSquare"},
+    {url: seventy, id: 70, description: "twoGreenSolidCircle"},
+    {url: seventyOne, id: 71, description: "twoGreenSolidRectangle"},
+    {url: seventyTwo, id: 72, description: "twoGreenSolidSquare"},
+    {url: seventyThree, id: 73, description: "twoRedClearCircle"},
+    {url: seventyFour, id: 74, description: "twoRedClearRectangle"},
+    {url: seventyFive, id: 75, description: "twoRedClearSquare"},
+    {url: seventySix, id: 76, description: "twoRedPatternCircle"},
+    {url: seventySeven, id: 77, description: "twoRedPatternRectangle"},
+    {url: seventyEight, id: 78, description: "twoRedPatternSquare"},
+    {url: seventyNine, id: 79, description: "twoRedSolidCircle"},
+    {url: eighty, id: 80, description: "twoRedSolidRectangle"},
+    {url: eightyOne, id: 81, description: "twoRedSolidSquare"},
+    
+
 ]
